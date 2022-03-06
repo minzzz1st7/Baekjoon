4c85bcf86d5607101af7d2f931a215092ea44a6c
minzzz1st7 <minzzz1st7@gmail.com>
2022-03-06 22:09:39 +0900

    RandomT

---
+++ src/Jansims/Alzal/BK8320_직사각형을만드는방법.java
@@ -0,0 +1,4 @@
+package Jansims.Alzal;
+
+public class BK8320_직사각형을만드는방법 {
+}
---
+++ src/TEST/Degree_01.java
@@ -0,0 +1,19 @@
+package TEST;
+
+public class Degree_01 {
+
+
+    public static void main(String[] args) {
+        int px1 = 0, py1 = 0;
+        int px2 = 2, py2 = 2;
+
+        float dx = px1 - px2;
+        float dy = py1 - py2;
+
+        double radian = Math.atan(dx/dy);
+        float degree = (float) (57.29 * radian);
+
+        System.out.println(degree);
+    }
+
+}
