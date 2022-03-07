d0435a9e41a49a23dfe3b8f8094a6a61dbbe6d49
minzzz1st7 <minzzz1st7@gmail.com>
2022-03-07 21:48:30 +0900

    RandomT

--- src/Jansims/BK10828_스택.java
+++ src/Jansims/BK10828_스택.java
@@ -20,36 +20,35 @@ public class BK10828_스택 {
 
          if(Ins.equals("push")){
              int num = Integer.parseInt(st.nextToken());
-
              stack.push(num);
          }
 
          else if(Ins.equals("top")){
-
+             if(stack.isEmpty()){
+                 System.out.println(-1);
+             } else {
+                 System.out.println(stack.get(stack.size()-1));
+             }
          }
 
          else if(Ins.equals("size")){
-             System.out.println(stack.size());
+             System.out.println( stack.size());
          }
          else if(Ins.equals("pop")){
              if(stack.isEmpty()){
                  System.out.println(-1);
+             } else{
+                 System.out.println(stack.pop());
              }
-             System.out.println(stack.pop());
-
          }
          else if(Ins.equals("empty")){
-             if(Ins.isEmpty()){
+             if(stack.isEmpty()){
                  System.out.println(1);
              } else {
-                 System.out.println(0);
+                 System.out.println( 0);
              }
          }
-
      }
-
-
-
      br.close();
     }
 }
