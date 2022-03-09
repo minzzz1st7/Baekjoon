45fae3751399382bdfe14d0311965bb066e987f5
minzzz1st7 <minzzz1st7@gmail.com>
2022-03-09 22:16:28 +0900

    RandomT

---
+++ src/Jansims/BK1764_듣보잡_JJ.java
@@ -0,0 +1,44 @@
+package Jansims;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BK1764_듣보잡_JJ {
+    public static void main(String[] args) throws IOException {
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        String[] name = new String[N];
+        for(int i = 0; i < N+M; i++){
+            name[i] = br.readLine();
+
+        }
+        int cnt = 0;
+        ArrayList<String> arr = new ArrayList<>();
+        for(int j= N+2 ; j < M; j++){
+            String see = br.readLine();
+            for(int i= 0 ; i < N; i++){
+                if(name[i].equals(see)){
+                    arr.add(see);
+                    cnt++;
+            }
+
+            }
+        }
+        arr.sort(String::compareToIgnoreCase);
+
+        System.out.println(cnt);
+        for(String x : arr){
+            System.out.println(x);
+        }
+
+
+     br.close();
+    }
+}
--- src/TEST/Pow.java
+++ src/TEST/Pow.java
@@ -1,4 +1,10 @@
 package TEST;
 
 public class Pow {
+    public static void main(String[] args) {
+        int num1 = 3;
+        for (int i = 1; i <= 10; i++){
+            System.out.printf("Math.pow(%d, %d) => %f \n", num1, i, Math.pow(num1, i));
+        }
+    }
 }
