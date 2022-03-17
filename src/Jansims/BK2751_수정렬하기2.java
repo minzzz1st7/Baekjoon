eb8f02d9620f75915d5cfa7e38e0857a8daeecc1
minzzz1st7 <minzzz1st7@gmail.com>
2022-03-17 23:19:38 +0900

    RandomT

--- src/Jansims/BK2751_수정렬하기2.java
+++ src/Jansims/BK2751_수정렬하기2.java
@@ -15,22 +15,22 @@ import java.util.PriorityQueue;
 
 public class BK2751_수정렬하기2 {
     public static void main(String[] args) throws IOException {
-     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-     StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-     int N = Integer.parseInt(br.readLine());
-     PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
 
-     for (int n = 0; n < N; n++){
-         heap.add(Integer.parseInt(br.readLine()));
+        for (int n = 0; n < N; n++) {
+            heap.add(Integer.parseInt(br.readLine()));
 
 
+            for (int i = 0; i < N; i++) {
+                sb.append(heap.poll()).append("\n");
+            }
+            System.out.println(sb);
 
-     for(int i = 0; i < N; i++){
-        sb.append(heap.poll()).append("\n");
-     }
-     System.out.println(sb);
-
-     br.close();
+            br.close();
+        }
     }
 }
