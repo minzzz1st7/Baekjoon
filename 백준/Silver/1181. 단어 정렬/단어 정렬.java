0574db96c19d7045d8f0aa3d2e39c8d78a5e5717
MinJi Jeong <70947171+minzzz1st7@users.noreply.github.com>
2022-03-22 22:47:06 +0900

    [Silver V] Title: 단어 정렬, Time: 924 ms, Memory: 35464 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		HashSet<String> str1=new HashSet<>();
+		for(int i=0;i<n;i++) {
+			String A="";
+			A=scan.next();
+			str1.add(A);
+		}
+
+		ArrayList<String> str2=new ArrayList<>(str1); //set을 ArrayList에 담는다
+
+		Collections.sort(str2,new Comparator<String>() {
+			public int compare(String s1,String s2) { 
+				if(s1.length()>s2.length())  //문자 길이로 정렬
+					return 1;
+				else if(s1.length()<s2.length())
+					return -1;
+				else // 길이가 같은 문자는 사전순으로 정렬 
+					return s1.compareTo(s2);
+			}
+		});
+		
+		for(String s:str2) {
+			System.out.println(s);
+		}
+	}
+}
