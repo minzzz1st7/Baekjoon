1e6d8d91d348cffc8056eb00369e7a9f368a587b
MinJi Jeong <70947171+minzzz1st7@users.noreply.github.com>
2022-04-18 04:35:44 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 76 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+        int max = 0;
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            max = max < arr[i] ? arr[i] : max;
+        } // input
+
+        for(int i = 0; i < N; i++){
+            int dp[] = new int[max+1];
+
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+
+            for(int n = 4; n <= max; n++){
+                dp[n] = dp[n-1] + dp[n-2] + dp[n-3];
+
+            }
+            System.out.println(dp[arr[i]]);
+        }
+
+        br.close();
+    }
+
+}
