a4db54b204af10b33c74c5f75c90b8bfe4fd4294
MinJi Jeong <70947171+minzzz1st7@users.noreply.github.com>
2022-04-17 20:23:02 +0900

    [Silver IV] Title: 13일의 금요일, Time: 128 ms, Memory: 13108 KB -BaekjoonHub

---
+++ 백준/Silver/16463. 13일의 금요일/13일의 금요일.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int day[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+        // 0: Sun, 1: Mon, 2: Tues, 3: Wed, 4: Thur, 5: Fri, 6: Sat
+        int start = 2;
+        int cnt = 0;
+        for(int year = 2019; year <= N; year++){
+            for(int month = 1; month <=12; month++) {
+                if ((start + 12) % 7 == 5) {
+                    cnt++;
+                }
+
+                if( month==2 && (year % 400 == 0 || (year % 100 != 0 && year % 4 ==0))){
+                    start++;
+                }
+                start = (start+day[month]) % 7;
+            }
+        }
+
+        System.out.println(cnt);
+
+        sc.close();
+    }
+}
