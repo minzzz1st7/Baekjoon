f19def2868dea8a37ccfcee6a91cc1ba8256ca13
MinJi Jeong <70947171+minzzz1st7@users.noreply.github.com>
2022-04-18 04:26:39 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 452 ms, Memory: 41828 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    static int N, ans = 0, max = 0;
+    static int[] X = {-1,0,1,0};
+    static int[] Y = {0,1,0,-1};
+    static int[][] map, dp;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        dp = new int[N][N];
+
+        for(int i = 0; i < N; i++) {
+            Arrays.fill(dp[i], 0);
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if(dp[i][j] != 0)
+                    continue;
+                dp[i][j] = 1;
+                ans = Math.max(ans, dfs(i, j));
+            }
+        }
+        System.out.println(ans);
+    }
+
+    static int dfs(int x, int y) {
+        for(int i = 0; i < 4; i++) {
+            int nx = x + X[i];
+            int ny = y + Y[i];
+
+            if(nx < 0 || ny < 0 || nx >= N || ny >= N || map[nx][ny] <= map[x][y]){
+                continue;
+            }
+
+            if(dp[nx][ny] == 0) {
+                int cnt = dfs(nx, ny);
+                dp[x][y] = Math.max(dp[x][y], cnt + 1);
+            }else {
+                dp[x][y] = Math.max(dp[x][y], dp[nx][ny] + 1);
+            }
+        }
+
+        if(dp[x][y] == 0)
+            return 1;
+
+        return dp[x][y];
+    }
+}
