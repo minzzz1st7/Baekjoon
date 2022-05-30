aaff5fe3df73706a139a4adedbf057e86e083d13
정민지 <minzzz1st7@naver.com>
2022-05-30 12:17:09 +0900

    RandomT

--- src/Alzalzal/BK21966_중략.java
+++ src/Alzalzal/BK21966_중략.java
@@ -1,5 +1,12 @@
 package Alzalzal;
 
-public class BK21966_중략 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class BK21966_중략 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.close();
+    }
 }
