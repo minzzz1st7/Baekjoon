528dbe5b9b229e0241a985e34751181b86774c2d
정민지 <minzzz1st7@gmail.com>
2022-07-02 23:49:39 +0900

    RandomT

--- src/Alzalzal/BK14500_테트로미노.java
+++ src/Alzalzal/BK14500_테트로미노.java
@@ -1,4 +1,30 @@
 package Alzalzal;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BK14500_테트로미노 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int board[][] = new int[N][M];
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine()," ");
+            for(int j = 0; j < M; j++){
+                board[i][j] = Integer.parseInt(st.nextToken());
+
+            }
+        }
+
+
+        
+    }
 }
