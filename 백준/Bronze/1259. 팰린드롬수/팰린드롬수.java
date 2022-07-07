734916bae6ecbb3d234bde4c76e1eba11183d77f
MinJi Jeong <70947171+minzzz1st7@users.noreply.github.com>
2022-07-08 01:36:15 +0900

    [Bronze I] Title: 팰린드롬수, Time: 80 ms, Memory: 11576 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while(true){
+            String ss = br.readLine();
+            if(ss.equals("0")){
+                break;
+            }
+
+            String s[] = ss.split("");
+            String left = "";
+            String right = "";
+
+
+            if(s.length % 2 == 0){
+                int N = s.length/2 - 1;
+                for(int i = 0; i <= N; i++){
+                    left += s[i];
+                }
+                for(int j = s.length-1; j > N; j--){
+                    right += s[j];
+                }
+
+                if(left.equals(right)){
+                    sb.append("yes").append("\n");
+                }
+                else {
+                    sb.append("no").append("\n");
+                }
+
+            }
+            else {
+                int N = s.length /2;
+
+                for(int i = 0; i < N; i++){
+                    left += s[i];
+                }
+                for(int j = s.length-1; j > N; j--){
+                    right += s[j];
+                }
+
+                if(left.equals(right)){
+                    sb.append("yes").append("\n");
+                }
+                else {
+                    sb.append("no").append("\n");
+                }
+
+            }
+        }
+        System.out.println(sb);
+
+    }
+}
