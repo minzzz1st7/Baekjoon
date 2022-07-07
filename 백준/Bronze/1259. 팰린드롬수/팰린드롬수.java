303ea1217183254156d1dcadb007596c836c6d16
MinJi Jeong <70947171+minzzz1st7@users.noreply.github.com>
2022-07-08 01:51:05 +0900

    [Bronze I] Title: 팰린드롬수, Time: 76 ms, Memory: 11644 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -1,62 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
+        List<String> list = new ArrayList<>();
         while(true){
-            String ss = br.readLine();
-            if(ss.equals("0")){
+            String s = br.readLine();
+            if(s.equals("0")){
                 break;
             }
+            list.add(s);
+        }
 
-            String s[] = ss.split("");
-            String left = "";
-            String right = "";
+        for(String f : list){
+            String ans = "yes";
+            int N = f.length()/2;
+            String[] ss = f.split("");
 
-
-            if(s.length % 2 == 0){
-                int N = s.length/2 - 1;
-                for(int i = 0; i <= N; i++){
-                    left += s[i];
+            for(int i = 0; i < N; i++){
+                if(!ss[i].equals(ss[f.length() - 1 - i])){
+                    ans = "no";
+                    break;
                 }
-                for(int j = s.length-1; j > N; j--){
-                    right += s[j];
-                }
-
-                if(left.equals(right)){
-                    sb.append("yes").append("\n");
-                }
-                else {
-                    sb.append("no").append("\n");
-                }
-
             }
-            else {
-                int N = s.length /2;
 
-                for(int i = 0; i < N; i++){
-                    left += s[i];
-                }
-                for(int j = s.length-1; j > N; j--){
-                    right += s[j];
-                }
-
-                if(left.equals(right)){
-                    sb.append("yes").append("\n");
-                }
-                else {
-                    sb.append("no").append("\n");
-                }
-
-            }
+            sb.append(ans).append("\n");
         }
         System.out.println(sb);
-
     }
 }
