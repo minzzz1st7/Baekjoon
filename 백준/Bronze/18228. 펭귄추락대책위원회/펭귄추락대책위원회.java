8a80b83574ecae7ca3285f501528004fbb3cb55e
MinJi Jeong <70947171+minzzz1st7@users.noreply.github.com>
2022-07-07 23:47:01 +0900

    [Bronze II] Title: 펭귄추락대책위원회, Time: 368 ms, Memory: 48980 KB -BaekjoonHub

---
+++ 백준/Bronze/18228. 펭귄추락대책위원회/펭귄추락대책위원회.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] s = new int[N+1];
+        int pg = 0;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 1; i <= N; i++){
+            s[i]= Integer.parseInt(st.nextToken());
+            if (s[i] == -1){
+                pg = i;
+            }
+        }
+        int minleft = Integer.MAX_VALUE;
+        int minright = Integer.MAX_VALUE;
+
+        for(int i = pg -1; i > 0; i-- ){
+            minleft = minleft > s[i] ? s[i] : minleft;
+        }
+
+        for(int i = pg +1; i <= N; i++){
+            minright = minright > s[i] ? s[i] : minright;
+        }
+
+        System.out.println(minleft + minright);
+        br.close();
+    }
+}
