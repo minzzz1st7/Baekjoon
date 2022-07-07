1c851ab1e469738fa0fa365825fab6bbb36fc8ac
MinJi Jeong <70947171+minzzz1st7@users.noreply.github.com>
2022-07-08 00:00:15 +0900

    [Bronze II] Title: 펭귄추락대책위원회, Time: 356 ms, Memory: 50260 KB -BaekjoonHub

--- 백준/Bronze/18228. 펭귄추락대책위원회/펭귄추락대책위원회.java
+++ 백준/Bronze/18228. 펭귄추락대책위원회/펭귄추락대책위원회.java
@@ -1,36 +1,31 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+public class Main {
 
-        int N = Integer.parseInt(br.readLine());
-        int[] s = new int[N+1];
-        int pg = 0;
-
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 1; i <= N; i++){
-            s[i]= Integer.parseInt(st.nextToken());
-            if (s[i] == -1){
-                pg = i;
-            }
-        }
-        int minleft = Integer.MAX_VALUE;
-        int minright = Integer.MAX_VALUE;
-
-        for(int i = pg -1; i > 0; i-- ){
-            minleft = minleft > s[i] ? s[i] : minleft;
-        }
-
-        for(int i = pg +1; i <= N; i++){
-            minright = minright > s[i] ? s[i] : minright;
-        }
-
-        System.out.println(minleft + minright);
-        br.close();
-    }
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int lmin=Integer.MAX_VALUE,rmin=Integer.MAX_VALUE;
+		boolean chk=false;
+		while(st.hasMoreTokens())
+		{
+			int tmp=Integer.parseInt(st.nextToken());
+			if(tmp==-1) {
+				chk=true;
+				continue;
+			}
+			if(!chk) {
+				if(tmp<lmin)
+					lmin=tmp;
+			}
+			else {
+				if(tmp<rmin)
+					rmin=tmp;
+			}
+		}
+		System.out.println(rmin+lmin);
+	}
 }
