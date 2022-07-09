c1caff5f861c7ac2b2be3930337d42141f186509
MinJi Jeong <70947171+minzzz1st7@users.noreply.github.com>
2022-07-10 04:09:36 +0900

    [Silver III] Title: 달팽이, Time: 452 ms, Memory: 69820 KB -BaekjoonHub

---
+++ 백준/Silver/1913. 달팽이/README.md
@@ -0,0 +1,12 @@
+# [Silver III] 달팽이 - 1913 
+
+[문제 링크](https://www.acmicpc.net/problem/1913) 
+
+### 성능 요약
+
+메모리: 69820 KB, 시간: 452 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Silver/1913. 달팽이/달팽이.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Main {
+    static int N = 0, M = 0;
+
+    static int dx[] = { -1, 0 ,1, 0};
+    static int dy[] = {0 , 1, 0, -1};
+
+    static int[][] arr;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        int ansX = 0;
+        int ansY = 0;
+
+        arr = new int[N][N];
+        board();
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                sb.append(arr[i][j]).append(" ");
+                if(arr[i][j] == M){
+                    ansX = i+1;
+                    ansY = j+1;
+                }
+            }
+            sb.append("\n");
+        }
+        
+        sb.append(ansX + " " + ansY);
+
+        System.out.println(sb);
+    }
+    static int cnt = 1, max = 2, two = 0, next = 0;
+    public static void board() {
+        int X = N/2;
+        int Y = N/2;
+
+        for(int i = 1; i <= N*N; i++){
+            arr[X][Y] = i;
+      
+
+            X = X + dx[next % 4];
+            Y = Y + dy[next % 4];
+
+            cnt ++;
+
+            if(cnt == max){
+                cnt = 1;
+                two++;
+                next ++;
+            }
+
+            if( two == 2){
+                max ++;
+                two = 0;
+            }
+        }
+    }
+}
